10da72b48493f5425577653dc02d0b0381b0d2f4
kadhaDeemi <dami.valderrama@duocuc.cl>
2025-07-29 15:28:04 -0400

    ADD implementation in educacion card certificados and panel lateral, changes in index

--- src/educacion.jsx
+++ src/educacion.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 
 export default function Educacion() {
-  const [selectedCard, setSelectedCard] = useState(null);
+  // Nuevo estado para controlar qué contenido se muestra en el panel lateral
+  // Puede ser 'cursos', 'certificaciones', o null (para panel cerrado)
+  const [selectedPanelContent, setSelectedPanelContent] = useState(null);
   const [selectedCategory, setSelectedCategory] = useState("Todos");
-  
 
   // Cursos agrupados por categoria
   const cursos = {
@@ -58,27 +59,94 @@ export default function Educacion() {
     "Gestión & Metodologías": "text-orange-400",
   };
 
+  // --- DATA PARA CERTIFICACIONES ---
+  const certificaciones = [
+    {
+      titulo: "ANÁLISIS Y DESARROLLO DE MODELOS DE DATOS",
+      institucion: "Instituto Profesional Duoc UC",
+      fecha: "Obtenido en Diciembre 2024",
+      descripcion: "Valida la capacidad para diseñar, optimizar e implementar estructuras de bases de datos robustas y eficientes, asegurando la integridad y disponibilidad de la información.",
+      enlace: "https://drive.google.com/file/d/1_XsatMGRVigqchQOikI8sZ_9RgB8rlXX/view?usp=sharing",
+    },
+    {
+      titulo: "CALIDAD DE SOFTWARE",
+      institucion: "Instituto Profesional Duoc UC",
+      fecha: "Obtenido en Diciembre 2024",
+      descripcion: "Acredita conocimientos en metodologías y herramientas para asegurar la excelencia, fiabilidad y eficiencia de los productos de software a lo largo de su ciclo de vida.",
+      enlace: "https://drive.google.com/file/d/1_XsatMGRVigqchQOikI8sZ_9RgB8rlXX/view?usp=sharing", 
+    },
+    {
+      titulo: "ARQUITECTURA DE SOFTWARE",
+      institucion: "Instituto Profesional Duoc UC",
+      fecha: "Obtenido en Diciembre 2024",
+      descripcion: "Certifica la habilidad para diseñar estructuras de sistemas de software escalables, mantenibles y seguras, definiendo los componentes clave y sus interacciones.",
+      enlace: "https://drive.google.com/file/d/1_XsatMGRVigqchQOikI8sZ_9RgB8rlXX/view?usp=sharing",
+    },
+    {
+        titulo: "INTERNACIONAL EN INGLÉS INTERMEDIO ALTO",
+        institucion: "Instituto Profesional Duoc UC",
+        fecha: "Obtenido en Diciembre 2024",
+        descripcion: "Demuestra fluidez y competencia comunicativa en inglés a un nivel intermedio-alto, facilitando la interacción en entornos profesionales globales y el acceso a recursos técnicos.",
+        enlace: "https://drive.google.com/file/d/1_XsatMGRVigqchQOikI8sZ_9RgB8rlXX/view?usp=sharing",
+    },
+    {
+        titulo: "GESTIÓN DE PROYECTOS INFORMÁTICOS",
+        institucion: "Instituto Profesional Duoc UC",
+        fecha: "Obtenido en Diciembre 2024",
+        descripcion: "Valida la experiencia en la planificación, ejecución y control de proyectos de TI, aplicando las mejores prácticas para asegurar la entrega exitosa dentro del alcance, tiempo y presupuesto.",
+        enlace: "https://drive.google.com/file/d/1_XsatMGRVigqchQOikI8sZ_9RgB8rlXX/view?usp=sharing",
+    },
+    {
+        titulo: "ANÁLISIS Y PLANIFICACIÓN DE REQUERIMIENTOS INFORMÁTICOS",
+        institucion: "Instituto Profesional Duoc UC",
+        fecha: "Obtenido en Diciembre 2024",
+        descripcion: "Descripción: Acredita la capacidad para identificar, documentar y gestionar los requerimientos de sistemas de software, traduciendo las necesidades del negocio en especificaciones funcionales y no funcionales.",
+        enlace: "https://drive.google.com/file/d/1_XsatMGRVigqchQOikI8sZ_9RgB8rlXX/view?usp=sharing",
+    },
+    {
+        titulo: "INTELIGENCIA DE NEGOCIOS",
+        institucion: "Instituto Profesional Duoc UC",
+        fecha: "Obtenido en Diciembre 2024",
+        descripcion: "Certifica habilidades en la transformación de datos en información estratégica y conocimiento para la toma de decisiones, mediante el uso de herramientas y técnicas de Business Intelligence.",
+        enlace: "https://drive.google.com/file/d/1_XsatMGRVigqchQOikI8sZ_9RgB8rlXX/view?usp=sharing",
+    },
+    {
+        titulo: "PROGRAMACIÓN DE SOFTWARE",
+        institucion: "Instituto Profesional Duoc UC",
+        fecha: "Obtenido en Diciembre 2024",
+        descripcion: "Demuestra competencias fundamentales y avanzadas en el desarrollo, codificación y depuración de aplicaciones de software, siguiendo las mejores prácticas de programación.",
+        enlace: "https://drive.google.com/file/d/1_XsatMGRVigqchQOikI8sZ_9RgB8rlXX/view?usp=sharing",
+    },
+    {
+        titulo: "GESTIÓN DE PROYECTOS TI",
+        institucion: "Instituto Profesional Duoc UC",
+        fecha: "Obtenido en Diciembre 2024",
+        descripcion: "Certificación que valida la aprobación de la Especialización en Gestión de Proyectos de Tecnologías de la Información, cubriendo las metodologías y prácticas para la dirección exitosa de iniciativas TI.",
+        enlace: "https://drive.google.com/file/d/1P3rYHefhJfufKTdPeGKxGm3Y3XWqzPEj/view?usp=sharing",
+    },
+  ];
+
   return (
     <div className="w-full relative">
-      <div className="max-w-5xl mx-auto mt-5 px-10">
-        <ul>
+      <div className="max-w-5xl mx-auto mt-3 px-10">
+        <ul className="relative"> {/*para los panelees*/}
           {[1, 2].map((semester) => (
             <li
               key={semester}
               data-aos="fade"
               id={`card${semester}`}
-              className="relative mb-12"
+              className="relative mb-10"
             >
               {/* Card principal */}
               <div
                 onClick={() => {
                   if (semester === 1) {
-                    setSelectedCard(semester);
+                    setSelectedPanelContent('cursos');
                   } else {
-                    setSelectedCard(null);
+                    setSelectedPanelContent(null);
                   }
                 }}
-                className="cursor-pointer flex flex-col justify-between h-auto group overflow-hidden transition-colors duration-500 ease-in-out  
+                className="cursor-pointer flex flex-col justify-between h-auto group overflow-hidden transition-colors duration-500 ease-in-out
                 bg-[#131313] p-6 border-[1.6px] border-[#2c2c2c] shadow-lg rounded-xl hover:border-orange-800 relative"
               >
                 <div>
@@ -174,75 +242,148 @@ export default function Educacion() {
                   </div>
                   <div
                   className="h-2 w-8 rounded-full shadow-lg
-                  bg-gradient-to-r from-[#1e3a8a] via-[#b91c1c] to-[#1e3a8a] 
+                  bg-gradient-to-r from-[#1e3a8a] via-[#b91c1c] to-[#1e3a8a]
                   bg-[length:300%_300%] animate-neon-blue-red"
                   style={{ width: "100%" }}>
                   </div>
                 </div>
               </div>
-
-              {/* Panel lateral */}
-              {selectedCard === semester && (
-                <div className="absolute top-0 left-full ml-6 w-80 rounded-xl shadow-xl p-[2px] overflow-hidden animate-slideIn">
-                  {/* Fondo gradiente neon */}
-                  <div className="absolute -inset-[2px] rounded-xl opacity-100 pointer-events-none 
-                      bg-gradient-to-r from-[#1e3a8a] via-[#b91c1c] to-[#1e3a8a]
-                      bg-[length:300%_300%] animate-neon-blue-red z-0"></div>
-
-                  {/* Contenido del panel */}
-                  <div className="relative z-10 bg-[#1a1a1a] rounded-xl p-4 text-white">
-                    {/* Header*/}
-                    <div className="flex justify-between items-center mb-4">
-                      <h4 className="text-lg font-semibold text-orange-400">
-                        Cursos Destacados
-                      </h4>
-                      <button
-                        onClick={() => setSelectedCard(null)}
-                        className="text-gray-400 hover:text-white"
-                      >
-                        ✕
-                      </button>
-                    </div>
-
-                    {/* filtro */}
-                    <select
-                      value={selectedCategory}
-                      onChange={(e) => setSelectedCategory(e.target.value)}
-                      className="w-full p-2 rounded-md bg-[#222] text-gray-300 border border-[#333] mb-3"
-                    >
-                      {categorias.map((cat) => (
-                        <option key={cat} value={cat}>
-                          {cat}
-                        </option>
-                      ))}
-                    </select>
-
-                    {/* List */}
-                    <div className="space-y-3 max-h-74 overflow-y-auto pr-3">
-                      {getCursosFiltrados().map(({ categoria, curso }, index) => (
-                        <div
-                          key={index}
-                          className={`relative p-3 rounded-xl bg-[#222] hover:bg-[#2c2c2c] transition group border border-transparent`}
-                        >
-                          {/* Borde neon al hover */}
-                          <span className="absolute -inset-[2px] rounded-2xl opacity-0 pointer-events-none 
-                              bg-gradient-to-r from-[#1e3a8a] via-[#b91c1c] to-[#1e3a8a]
-                              bg-[length:300%_300%] animate-neon-blue-red 
-                              transition-opacity duration-500 group-hover:opacity-100 z-[-1]"></span>
-
-                          <p className="text-sm text-gray-200">{curso}</p>
-                          <span className={`text-xs ${categoriaColor[categoria]}`}>
-                            {categoria}
-                          </span>
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-              )}
             </li>
           ))}
+          {/*Div certificaciones */}
+          <li className="relative mb-2">
+            <div className="cursor-pointer flex flex-col justify-between h-auto group overflow-hidden transition-colors duration-500 ease-in-out
+                            bg-[#131313] p-6 border-[1.6px] border-[#2c2c2c] shadow-lg rounded-xl hover:border-blue-600 relative"
+                 onClick={() => setSelectedPanelContent(selectedPanelContent === 'certificaciones' ? null : 'certificaciones')}>
+                <div className="flex items-center gap-3 mb-2">
+                    <div className="p-3 bg-[#1a1a1a] rounded-lg">
+                        <img src="/certificado.gif"className="w-8 h-8" />
+                    </div>
+                    <div>
+                        <h3 className="text-4xl font-bold text-white">
+                            Ver Certificaciones ↗
+                        </h3>
+                        <span className="text-yellow-400 text-xl font-semibold">
+                            Explora mis logros
+                        </span>
+                    </div>
+                </div>
+                <div className="mt-">
+                  <div className="flex items-center justify-between text-sm mb-1">
+                    <span className="font-semibold text-gray-200">
+                      Haz click para ver los detalles
+                    </span>
+                  </div>
+                  <div
+                  className="h-2 w-full rounded-full shadow-lg
+                  bg-gradient-to-r from-[#1e3a8a] via-[#b91c1c] to-[#1e3a8a]
+                  bg-[length:300%_300%] animate-neon-blue-red"
+                  ></div>
+                </div>
+            </div>
+          </li>
         </ul>
+
+        {/* PANEL LATERAL CURSO/CERTIFICACIONES*/}
+        {selectedPanelContent && (
+          <div className={`absolute top-0 left-full ml-6 w-90 rounded-xl shadow-xl p-[2px] overflow-hidden animate-slideIn transition-transform duration-500 ease-in-out`}>
+            {/* Fondo gradiente neon */}
+            <div className="absolute -inset-[2px] rounded-xl opacity-100 pointer-events-none
+                bg-gradient-to-r from-[#1e3a8a] via-[#b91c1c] to-[#1e3a8a]
+                bg-[length:300%_300%] animate-neon-blue-red z-0"></div>
+
+            {/* Contenido del panel */}
+            <div className="relative z-10 bg-[#1a1a1a] rounded-xl p-4 text-white">
+              {/* Header del Panel */}
+              <div className="flex justify-between items-center mb-4">
+                <h4 className="text-lg font-semibold text-orange-400">
+                  {selectedPanelContent === 'cursos' ? "Cursos Destacados" : "Mis Certificaciones"}
+                </h4>
+                <button
+                  onClick={() => setSelectedPanelContent(null)}
+                  className="text-gray-400 hover:text-white"
+                >
+                  ✕
+                </button>
+              </div>
+
+              {/* Contenido condicional: Cursos o Certificaciones */}
+              {selectedPanelContent === 'cursos' && (
+                <>
+                  {/* filtro de cursos */}
+                  <select
+                    value={selectedCategory}
+                    onChange={(e) => setSelectedCategory(e.target.value)}
+                    className="w-full p-2 rounded-md bg-[#222] text-gray-300 border border-[#333] mb-3"
+                  >
+                    {categorias.map((cat) => (
+                      <option key={cat} value={cat}>
+                        {cat}
+                      </option>
+                    ))}
+                  </select>
+
+                  {/* Lista de Cursos */}
+                  <div className="space-y-3 max-h-[calc(100vh-200px)] overflow-y-auto pr-3"> {/* Ajustado max-h para mejor scroll */}
+                    {getCursosFiltrados().map(({ categoria, curso }, index) => (
+                      <div
+                        key={index}
+                        className={`relative p-3 rounded-xl bg-[#222] hover:bg-[#2c2c2c] transition group border border-transparent`}
+                      >
+                        {/* Borde neon al hover */}
+                        <span className="absolute -inset-[3px] rounded-2xl opacity-0 pointer-events-none
+                            bg-gradient-to-r from-[#1e3a8a] via-[#b91c1c] to-[#1e3a8a]
+                            bg-[length:300%_300%] animate-neon-blue-red
+                            transition-opacity duration-500 group-hover:opacity-100 z-[-1]"></span>
+
+                        <p className="text-sm text-gray-200">{curso}</p>
+                        <span className={`text-xs ${categoriaColor[categoria]}`}>
+                          {categoria}
+                        </span>
+                      </div>
+                    ))}
+                  </div>
+                </>
+              )}
+
+              {selectedPanelContent === 'certificaciones' && (
+                <div className="space-y-6 max-h-[calc(100vh-150px)] overflow-y-auto pr-3"> 
+                  {certificaciones.map((cert, index) => (
+                    <div
+                      key={index}
+                      className="group relative bg-[#131313] p-5 border-[1.6px] border-[#2c2c2c] shadow-lg rounded-xl
+                                 transition-colors duration-500 flex flex-col justify-between">
+                        <span className="absolute -inset-[5px] rounded-2xl opacity-0 pointer-events-none
+                            bg-gradient-to-r from-[#1e3a8a] via-[#b91c1c] to-[#1e3a8a]
+                            bg-[length:300%_300%] animate-neon-blue-red
+                            transition-opacity duration-500 group-hover:opacity-100 z-[-1]">
+                            </span>
+                      <div>
+                        <h3 className="text-lg font-bold text-white mb-1">{cert.titulo}</h3>
+                        <p className="text-gray-400 text-sm mb-1">{cert.institucion}</p>
+                        <p className="text-gray-500 text-xs mb-3">{cert.fecha}</p>
+                        {cert.descripcion && (
+                          <p className="text-gray-300 text-sm mb-3">{cert.descripcion}</p>
+                        )}
+                      </div>
+                      {cert.enlace && (
+                        <div className="mt-auto pt-2"> {/* Empuja el botón hacia abajo si el contenido varía */}
+                          <a
+                            href={cert.enlace}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="inline-block px-4 py-2 bg-orange-500 text-white rounded-md text-sm font-semibold
+                                       hover:bg-orange-700 transition-colors duration-300"> Ver Credencial
+                          </a>
+                        </div>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/home.jsx
+++ src/home.jsx
@@ -14,10 +14,10 @@ export default function Principal(){
                   <img src="/saludo.gif" alt="emoji gif" className="w-10 h-10" />
                    Bienvenido/a</h2>
                 <p className="mt-3 text-gray-300">
-                  Soy <span className="font-bold">Damian Valderrama</span>, Titulado de la <span className="font-bold">carrera Ingeniería en Informática</span> en <span className="text-yellow-300">Duoc UC</span> apasionado por el desarrollo Frontend. 
-                  Con 4 años de estudio completados y mi práctica profesional realizada, me encuentro activamente buscando mi primera oportunidad laboral, 
-                  cuento con experiencia en desarrollo <span className="font-bold">Frontend</span> y algo de <span className="font-bold">Backend</span>.
-                </p>
+                  Soy <span className="font-bold">Damian Valderrama Acuña</span>, <span className="font-bold">Ingeniero en Informática</span> titulado de <span className="text-yellow-300">Duoc UC</span>. 
+                  Cuento con sólida formación y experiencia práctica en desarrollo web, utilizando Python, Django, JavaScript (JSX) y React, además de bases de datos relacionales y no relacionales. 
+                  Estoy buscando activamente mi primera oportunidad profesional en desarrollo de software/web, donde pueda aportar mis conocimientos, compromiso y visión emprendedora.
+                  </p>
               <span className="absolute -inset-[2px] rounded-xl opacity-0 pointer-events-none
                                bg-gradient-to-r from-[#1e3a8a] via-[#b91c1c] to-[#1e3a8a]
                                bg-[length:300%_300%] animate-neon-blue-red
@@ -31,17 +31,19 @@ export default function Principal(){
                 <img src="/informatico.gif" alt="emoji gif" className="w-10 h-10" />
                 Habilidades
               </h2>
-              <ul className="grid grid-cols-1 gap-2 text-gray-300">
-                {/*<span className="text-sm sm:text-base font-medium text-black dark:text-white"> SQL</span>*/}
-                <li className="text-sm sm:text-base font-medium text-black dark:text-white" >✅ React.js</li>
-                <li>✅ Tailwind CSS</li>
-                <li>✅ Django / APIs</li>
-                <li>✅ PostgreSQL</li>
-                <li>✅ Git / GitHub</li>
-                <li>✅ Metodologías ágiles</li>
+              <ul className="grid grid-cols-1 pl-5 gap-2 text-gray-100 text-sm sm:text-base font-medium list-disc">
+                <li>React.js</li>
+                <li>Django</li>
+                <li>Python</li>
+                <li>JavaScript</li>
+                <li>SQL</li>
+                <li>Tailwind CSS</li>
+                <li>HTML & CSS</li>
+                <li>Git / GitHub</li>
+                <li>Metodologías ágiles</li>
               </ul>
-              <div className="mt-6 p-3 rounded-lg bg-[#1a1a1a] border-l-4 border-blue-600"> 
-                <p className="text-sm text-gray-300 italic" data-cursor="text">DJAKSJA
+              <div className="mt-7 p-3 rounded-lg bg-[#1a1a1a] border-l-3 border-violet-600"> 
+                <p className="text-sm text-gray-300 italic" data-cursor="text">Cuando no estoy programando, disfruto ver y jugar fútbol, jugar videojuegos. Y también ir al estadio a ver a la U de Chile🦉💙
                 </p>
               </div>
               <span className="absolute -inset-[2px] rounded-xl opacity-0 pointer-events-none
@@ -57,12 +59,17 @@ export default function Principal(){
                             transition-transform duration-300 ease-in-out hover:scale-[1.03] hover:shadow-2xl group">
               <h3 className="text-lg font-bold flex item-center gap-2"> 
               <img src="/buho.gif" alt="emoji gif" className="w-10 h-10" />
-                Educación</h3>
-              <p className="text-gray-300 text-sm mt-2">
-                Titulado de Ingeniería en Informática en DuocUC. 
-                Formación sólida en desarrollo web Frontend y Backend, 
-                con experiencia en proyectos académicos y prácticas profesionales.
+                Educación ↗</h3>
+              <p className="text-gray-200 text-lg mt-10  font-semibold">
+                Ingeneria en informatica
               </p>
+              <p className="text-gray-300 text-sm font-semibold">
+                DUOC UC 
+              </p>
+              <p className="text-gray-300 text-sm font-semibold">
+                2020 - 2024
+              </p>
+              <p class="underline font-semibold text-white-500 hover:text-gray-300 mt-2">ver más</p>
               <span className="absolute -inset-[2px] rounded-xl opacity-0 pointer-events-none
                                bg-gradient-to-r from-[#1e3a8a] via-[#b91c1c] to-[#1e3a8a]
                                bg-[length:300%_300%] animate-neon-blue-red
@@ -100,8 +107,8 @@ export default function Principal(){
             {/* Card 6: col 2, fila 6 */}
             <div className="col-start-2 col-span-1 row-start-8 col-span-1 relative rounded-xl border border-gray-700 p-4 bg-[#141418] text-white
                             transition-transform duration-300 ease-in-out hover:scale-[1.03] hover:shadow-2xl group">
-              <h4 className="font-bold">Card 6</h4>
-              <p className="text-sm text-gray-300">LA U</p>
+              <h4 className="font-bold">Próximamente...</h4>
+              <p className="text-sm text-gray-300"></p>
               <span className="absolute -inset-[2px] rounded-xl opacity-0 pointer-events-none
                                bg-gradient-to-r from-[#1e3a8a] via-[#b91c1c] to-[#1e3a8a]
                                bg-[length:300%_300%] animate-neon-blue-red
@@ -123,8 +130,8 @@ export default function Principal(){
             {/* Card 8: col 2, fila 7 */}
             <div className="col-start-3 col-span-1 row-start-8 row-span-1 relative rounded-xl border border-gray-700 p-4 bg-[#141418] text-white
                             transition-transform duration-300 ease-in-out hover:scale-[1.03] hover:shadow-2xl group">
-              <h4 className="font-bold">Card 8</h4>
-              <p className="text-sm text-gray-300">KJSAJKA</p>
+              <h4 className="font-bold">Próximamente....</h4>
+              <p className="text-sm text-gray-300"></p>
               <span className="absolute -inset-[2px] rounded-xl opacity-0 pointer-events-none
                                bg-gradient-to-r from-[#1e3a8a] via-[#b91c1c] to-[#1e3a8a]
                                bg-[length:300%_300%] animate-neon-blue-red
